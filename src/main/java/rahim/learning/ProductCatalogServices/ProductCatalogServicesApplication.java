90ea65f6afd0fc80070f6381408671617e019dc9
RahimTS <rahimatme@gmail.com>
2024-12-28 16:07:23 +0530

    initial commit

---
+++ src/main/java/rahim/learning/ProductCatalogServices/ProductCatalogServicesApplication.java
@@ -0,0 +1,13 @@
+package rahim.learning.ProductCatalogServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rahim/learning/ProductCatalogServices/ProductCatalogServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package rahim.learning.ProductCatalogServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
