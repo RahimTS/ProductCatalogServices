b3632efb196f57cd3499b2a6075a534620088108
RahimTS <rahimatme@gmail.com>
2025-01-10 23:38:55 +0530

    schema versioning example

---
+++ src/main/java/rahim/learning/ProductCatalogServices/models/TestModel.java
@@ -0,0 +1,8 @@
+package rahim.learning.ProductCatalogServices.models;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class TestModel extends BaseModel {
+    private Integer numField;
+}
---
+++ src/main/resources/db/migration/v2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE st_user MODIFY help_request INT NOT NULL;
+
+ALTER TABLE st_user MODIFY user_type INT NULL;
